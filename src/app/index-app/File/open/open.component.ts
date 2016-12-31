f988c1fadc10c54c50a3c73caaf73221ba70645b
FA69.方齐快 <fangqk@jekunauto.com>
2016-12-31 18:06:58 +0800

    formdata

--- src/app/index-app/File/open/open.component.ts
+++ src/app/index-app/File/open/open.component.ts
@@ -12,6 +12,7 @@ import { DataService, FormControlService, FormDataBase  }  from '../../../core';
 export class OpenComponent implements OnInit {
     FormDatas : FormDataBase<any>[] = [];
     form: FormGroup;
+    lock = false;
     url = "../../mock-data/formData.json";
     constructor(
       private getDataService : DataService,
@@ -21,10 +22,23 @@ export class OpenComponent implements OnInit {
     getData(): void {
       this.getDataService
           .getFormData(this.url)
-          .subscribe(res => {this.FormDatas = res; console.log(res); this.form = this.getFormControlService.toFormGroup(this.FormDatas);});
+          .subscribe(res => {this.FormDatas = res; console.log(res);     
+          this.form = this.getFormControlService.toFormGroup(this.FormDatas);
+          this.lock = true; });
     }
     ngOnInit(): void{
-        debugger;
         this.getData();
+        
+         
+    }
+    ngAfterContentInit() { 
+        // this.form.setValue({"GSID":"SJFKSJFK"}); 
+        // this.form.controls["GSID"].value="AFJSKJFK";
+    }
+    test(){
+        console.log(this.form);
+         console.log(this.form.controls["GSID"].value);
+         this.form.controls["GSID"].setValue(2);
+         this.form.setValue({"GSID":2222,"GSMC":"YUANYUAN","GSJC":"13","GSYWMC":"abc","GJID":"zhongguo","SFID":"guangdong","CSID":"guangzhou","CKJB":"中心仓","YXBJ":1,"Date":new Date("2016-12-31")});
     }
 }
